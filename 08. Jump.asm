db474debed9e32b4c72c0b1325b04509b579300d
Md. Emran Hossen <92653363+emmranhossen@users.noreply.github.com>
2024-02-01 00:21:16 +0600

    Add files via upload

---
+++ 08. Jump.asm
@@ -0,0 +1,39 @@
+.model small
+.stack 100h
+.data
+ a db 'Jump Concept$'
+ b db 'Assembly Languages$'
+ c db 'Programming$'
+.code
+
+main proc
+
+    mov ax,@data
+    mov ds,ax
+    
+    mov ah,9
+    lea dx,a
+    int 21h
+    
+    mov ah,2
+    mov dl,10
+    int 21h
+    mov dl,13
+    
+    m:
+    mov ah,9
+    lea dx,b
+    int 21h
+    jmp n
+    
+    n:
+    mov ah,9
+    lea dx,c
+    int 21h
+    jmp exit
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
